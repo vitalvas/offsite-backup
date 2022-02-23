5dcb28d1927737b02b41a57857ea1bd45483af6f
Vitaliy Vasilenko <me@vitalvas.com>
2022-02-24 00:40:28 +0200

    add boot script

--- bin/configure-system.sh
+++ bin/configure-system.sh
@@ -6,6 +6,14 @@ if [ ! -f "/etc/systemd/system/media-meta.mount" ]; then
   systemctl enable media-meta.mount
 fi
 
+if [ ! -f "/etc/systemd/system/vault-boot.service" ]; then
+  cp /media/meta/offsite-backup/extra/vault-boot.service /etc/systemd/system/vault-boot.service
+  systemctl daemon-reload
+  systemctl enable vault-boot.service
+  systemctl start vault-boot.service
+fi
+
+
 fgrep -q '/dev/mapper/vault-data' /proc/mounts && {
   if [ ! -d "/media/data/logs" ]; then
     btrfs subvolume create /media/data/logs
